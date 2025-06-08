ccca2d4835fb8444092d3b048146fe6604c80e06
Melisa Mary Wanjiku <melisawanjiku93@gmail.com>
2025-06-08 23:57:07 +0300

    Complete continued functions lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+// 1. Function declaration
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+// 2. Function expression
+const mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+// 3. Function returning a function
+function wrapAdjective(style = "*") {
+  return function(adjective = "special") {
+    return `You are ${style}${adjective}${style}!`;
+  };
+}
+
